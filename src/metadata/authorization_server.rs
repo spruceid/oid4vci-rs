22b9f343535cbdd7be5f9e19d1b1e94ed722b4f7
Jacob <jacob.ward@spruceid.com>
2024-09-05 15:11:55 +0100

    Fmt

--- src/metadata/authorization_server.rs
+++ src/metadata/authorization_server.rs
@@ -190,8 +190,7 @@ impl AuthorizationServerMetadata {
         CM: CredentialConfigurationProfile,
     {
         let credential_issuer_authorization_server_metadata =
-            Self::discover_async(credential_issuer_metadata.credential_issuer(), http_client)
-                .await;
+            Self::discover_async(credential_issuer_metadata.credential_issuer(), http_client).await;
         let Some(grant_type) = grant_type else {
             // If grants is not present or is empty, the Wallet MUST determine the Grant Types the
             // Credential Issuer's Authorization Server supports using the respective metadata.
